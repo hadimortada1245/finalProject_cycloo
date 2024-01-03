034b0947f576563c2b121909965bb25685f5bdd6
Hadi Mortada <hadi.mortada@isae.edu.lb>
2024-01-03 20:59:31 +0200

    Start with the server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express =require('express');
+const cors=require('cors');
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+const PORT=5000;
+app.listen(PORT,()=>{
+    console.log(`Server is runing on port :${PORT}`)
+})
+
